0092ef0559caf698775fb36eff0f5778baf63a2b
ahmedsaber25 <ahmedsabershendy@gmail.com>
2023-08-15 11:41:05 -0700

    saberr

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,20 +1,24 @@
-#ifndef main_h
-	#define main_h
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ *File: main.h
+ *Author: Danigy
+ *Descption: header file that declares functions 
+*/
 
+void print_alphabet(void);
+void dani_gy(void);
+void jack_bauer(void);
+void print_alphabet_x10(void);
+void times_table();
+void print_to_98(int);
+void print_times_table(int);
+int _putchar(char);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int add(int n, int m);
+int print_last_digit(int n);
 
-	int _putchar(char);
-	void print_alphabet(void);
-	void print_alphabet_x10(void);
-	int _islower(int c);
-	int _isalpha(int c);
-	int print_sign(int n);
-	int _abs(int);
-	int print_last_digit(int);
-	void jack_bauer(void);
-	void times_table(void);
-	int add(int, int);
-	void print_to_98(int n);
-	void print_times_table(int n);
-
-
-	#endif
+#endif
